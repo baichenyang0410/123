23005de0feb5e5f92ec5ce59be102c13c56db867
wzk <wangzhikai@qq.com>
2019-11-07 11:14:26 +0800

    王志凯修改

--- src/main/java/Test/Test.java
+++ src/main/java/Test/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("hellowordxxxxxxxxxxxxxx");
 		System.out.println("王志凯");
+		System.out.println("sdada");
 	}
 
 }
