fb82a562e3391e0c3ffca6373f14ed5d8dcbd4f3
wzk <wangzhikai@qq.com>
2019-11-07 11:23:48 +0800

    全删

--- src/main/java/Test/Test.java
+++ src/main/java/Test/Test.java
@@ -3,14 +3,7 @@ package Test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("hellowordxxxxxxxxxxxxxx");
-		System.out.println("王志凯");
-
-		System.out.println("菊花蚕");
-
-		System.out.println("sdada");
 		
-		System.out.println("穆连超");
-
 	}
 
 }
